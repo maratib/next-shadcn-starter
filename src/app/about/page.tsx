df9552b90e0988c42350a8367b60414ca91de0f6
Maratib Ali Khan <maratib@gmail.com>
2024-05-12 21:41:55 +0500

    initial commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen">
       <div className="m-auto">
-        <h1>Welcome to Next 14 with Shadcn</h1>
-        <Button>Shadcn Button</Button>
+        <h1>About Page</h1>
       </div>
     </main>
   );
